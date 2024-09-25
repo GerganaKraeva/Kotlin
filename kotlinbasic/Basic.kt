0790db4eabb59abdeac7be613803526603b2c077
GerganaKraeva <gergana.evg.kraeva@gmail.com>
2024-09-25 13:14:13 +0300

    xample for class in Kotlin

---
+++ kotlinbasic/Basic.kt
@@ -0,0 +1,5 @@
+package com.example.kotlinbasic
+
+fun main() {
+    var myBook = Book("Carloson who lives on the roof","Astrid Lindgren" ,1955)
+}
---
+++ kotlinbasic/BookClass.kt
@@ -0,0 +1,5 @@
+package com.example.kotlinbasic
+
+class Book (val title: String, val author: String, val yearPublished: Int ){
+
+}
