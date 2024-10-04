3c7d9105cdb7289a149112aee718c8c6baf831e4
GerganaKraeva <gergana.evg.kraeva@gmail.com>
2024-10-04 16:51:07 +0300

    added ShopppingListItem

--- Apps/shoppinglistapp/ShoppingList.kt
+++ Apps/shoppinglistapp/ShoppingList.kt
@@ -13,9 +13,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -48,9 +53,10 @@ fun shoppingListApp() {
         verticalArrangement = Arrangement.Center
     ) {
         Button(
-            onClick = {},
+            onClick = {showDialog = true},
             modifier = Modifier.align(Alignment.CenterHorizontally)
         ) {
+//            val doubleNumber: (Int) -> Int = { it*2}
             Text("Add Item")
         }
         LazyColumn(
@@ -135,6 +141,16 @@ fun ShoppingListItem(
             )
     ) {
         Text(text = item.name, modifier = Modifier.padding(8.dp))
+        Text(text = "Qty: ${item.quantity}", modifier = Modifier.padding(8.dp))
+        Row(modifier = Modifier.padding(8.dp)) {
+            IconButton(onClick = onEditClick) {
+                Icon(imageVector = Icons.Default.Edit, contentDescription = null)
+            }
+            IconButton(onClick = onDeleteClick) {
+                Icon(imageVector = Icons.Default.Delete, contentDescription = null)
+            }
+        }
+
     }
 
 }
