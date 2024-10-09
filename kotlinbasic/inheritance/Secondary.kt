f5cb5b8331743cab5af1138049422d96dd9d5942
GerganaKraeva <gergana.evg.kraeva@gmail.com>
2024-10-09 13:39:35 +0300

    inheritance in Kotlin

---
+++ kotlinbasic/inheritance/Secondary.kt
@@ -0,0 +1,12 @@
+package com.example.inheritance
+
+//Derived Class of BaseClass
+//Inherits from the BaseClass
+
+class Secondary: BaseClass (){
+
+    override fun role() {
+//        super.role()
+        println("Knight of the House of BaseClass")
+    }
+}
