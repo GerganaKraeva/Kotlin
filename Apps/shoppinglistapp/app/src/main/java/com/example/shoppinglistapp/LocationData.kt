d9a1649c69506060ed550b652ffde5d0256e3422
GerganaKraeva <gergana.evg.kraeva@gmail.com>
2024-10-30 14:18:06 +0200

    added LocationSelectiionScreeen

---
+++ Apps/shoppinglistapp/app/src/main/java/com/example/shoppinglistapp/LocationData.kt
@@ -0,0 +1,17 @@
+package com.example.shoppinglistapp
+
+data class LocationData(
+    val latitude: Double,
+    val longitude: Double
+)
+
+data class GeocodingResponse(
+    val result: List<GeocodingResult>,
+    val status: String
+)
+
+data class GeocodingResult(
+    val formatted_address: String
+)
+
+
